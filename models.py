6c3c2598211db92925fec21e0a4e30931e862315
Eogtuc09100 <eogtuc0910@highgateschool.org.uk>
2023-02-26 10:52:34 +0000

    weekend hw

--- models.py
+++ models.py
@@ -9,34 +9,36 @@ Base = declarative_base()
 book_author = Table('book_author',
                         Base.metadata,
                         Column('id', Integer, primary_key=True),
-                        Column('book_id', ForeignKey('book.id')),
-                        Column('author_id', ForeignKey('author.id')),
-                        UniqueConstraint('book_id', 'author_id')
+                        Column('book_id', ForeignKey('book.book_id')),
+                        Column('author_id', ForeignKey('author.author_id')),
+                    UniqueConstraint('book_id', 'author_id')
                         )
 
 class Book(Base):
     __tablename__ = 'book'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    book_id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String, nullable=False)
     ISBN_number = Column(String, nullable=False)
     num_pages = Column(Integer, nullable=False)
     publication_date = Column(String)
-    publisher_id = Column(Integer, ForeignKey("publisher.id"), default=None)
-    authors = relationship("Book",
+    publisher_id = Column(Integer, ForeignKey("publisher.publisher.id"), default=None)
+    authors = relationship("Author",
                              secondary=book_author,
-                             back_populates="author")
+                             back_populates="works")
+
+    publisher = relationship("Publisher", back_populates="books")
 
     def __repr__(self):
         return f"<Book({self.title})>"
 
 class Author(Base):
     __tablename__ = 'author'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    author_id = Column(Integer, primary_key=True, autoincrement=True)
     first_name = Column(String, nullable=False)
     last_name = Column(String, nullable=False)
-    books = relationship("Book",
+    works = relationship("Book",
                               secondary=book_author,
-                              back_populates="book")
+                              back_populates="authors")
 
     def __repr__(self):
         return f"<Author({self.first_name} {self.last_name})>"
@@ -44,10 +46,10 @@ class Author(Base):
 
 class Publisher(Base):
     __tablename__ = 'publisher'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    publisher_id = Column(Integer, primary_key=True, autoincrement=True)
     publisher_name = Column(String, nullable=False)
-    books = relationship("Author",
-                              backref=backref("publisher"))
+    books = relationship("Book",
+                              back_populates="publisher")
 
     def __repr__(self):
         return f"<Publisher({self.publisher_name})>"
