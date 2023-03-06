1549f2caa9d4798e9c01b9d945112d89484fabdb
Eogtuc09100 <eogtuc0910@highgateschool.org.uk>
2023-03-06 11:53:44 +0000

    aSDSA

---
+++ tkintermvc/view.py
@@ -0,0 +1,100 @@
+
+import tkinter as tk
+from tkinter import ttk
+from controller import Controller
+
+
+class View(ttk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        # create widgets
+        # label
+        self.label = ttk.Label(self, text='Email:')
+        self.label.grid(row=1, column=0)
+
+        # email entry
+        self.email_var = tk.StringVar()
+        self.email_entry = ttk.Entry(self, textvariable=self.email_var, width=30)
+        self.email_entry.grid(row=1, column=1, sticky=tk.NSEW)
+
+        # save button
+        self.save_button = ttk.Button(self, text='Save', command=self.save_button_clicked)
+        self.save_button.grid(row=1, column=3, padx=10)
+
+        # message
+        self.message_label = ttk.Label(self, text='', foreground='red')
+        self.message_label.grid(row=2, column=1, sticky=tk.W)
+
+        # set the controller
+        self.controller = None
+
+    def set_controller(self, controller):
+        """
+        Set the controller
+        :param controller:
+        :return:
+        """
+        self.controller = controller
+
+    def save_button_clicked(self):
+        """
+        Handle button click event
+        :return:
+        """
+        if self.controller:
+            self.controller.save(self.email_var.get())
+
+    def show_error(self, message):
+        """
+        Show an error message
+        :param message:
+        :return:
+        """
+        self.message_label['text'] = message
+        self.message_label['foreground'] = 'red'
+        self.message_label.after(3000, self.hide_message)
+        self.email_entry['foreground'] = 'red'
+
+    def show_success(self, message):
+        """
+        Show a success message
+        :param message:
+        :return:
+        """
+        self.message_label['text'] = message
+        self.message_label['foreground'] = 'green'
+        self.message_label.after(3000, self.hide_message)
+
+        # reset the form
+        self.email_entry['foreground'] = 'black'
+        self.email_var.set('')
+
+    def hide_message(self):
+        """
+        Hide the message
+        :return:
+        """
+        self.message_label['text'] = ''
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        self.title('Tkinter MVC Demo')
+
+        # create a view and place it on the root window
+        view = View(self)
+        view.grid(row=0, column=0, padx=10, pady=10)
+
+        # create a controller
+        controller = Controller()
+
+        # set the controller to view
+        view.set_controller(controller)
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
